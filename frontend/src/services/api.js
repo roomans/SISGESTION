3a7a04b62245ac469a5c76228c9acf8eb62fbe0b
roomans <emersonjoel76@gmail.com>
2026-06-29 07:35:45 -0500

    Configurar frontend para producción

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
-  /*baseURL: "http://192.168.1.149:3001/api",*/
-});
+  baseURL: import.meta.env.VITE_API_URL,
+}); 
