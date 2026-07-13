ea7e603e8647e207602b40bda95e2855ddec3d8e
roomans <emersonjoel76@gmail.com>
2026-07-13 11:43:34 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'sisgestion.vercel.app' 
+    baseURL: 'https://sisgestion-backend.onrender.com' 
 });
 
 api.interceptors.request.use(
